c02b5995bdcd895a2d098ec62eaa50adfaeac677
Muktashim Billah <muktasim.elfatih@gmail.com>
2024-07-04 19:12:17 +0700

    tes

--- resources/views/homepage/beranda.blade.php
+++ resources/views/homepage/beranda.blade.php
@@ -6,6 +6,16 @@
       background-image: url("{{asset('img/bg/bg jumbotron 1.png')}}");
       background-size: cover;
    }
+
+   .index-kartu-1 img {
+      max-height: 150px;
+   }
+
+   .index-kartu-1 .index-text {
+      height: 130px;
+      overflow: hidden;
+      text-align: justify;
+   }
 </style>
 @endsection
 
@@ -20,13 +30,30 @@
 </div>
 <div class="product-device shadow-sm d-none d-md-block"></div>
 <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+
+<div class="container mt-5">
+
+   <h3 class="text-center">Jalur Pendakian Gunung Kerinci</h3>
+   <div class="row mt-3 index-kartu-1">
+      @for ($i = 1; $i <= 5; $i++) <div class="col-12 col-md-6 col-lg-4 mb-3">
+         <div class="card h-100">
+            <img src="{{ asset('img/sampel/sampel 2.png') }}" class="card-img-top" alt="Jalur Pendakian Kersik Tuo">
+            <div class="card-body">
+               <h5 class="card-title">Jalur Kersik Tuo <span class="ms-2 badge gk-bg-primary400">Primary</span></h5>
+               <p class="card-text index-text">Lorem ipsum dolor sit amet consectetur. Lorem posuere amet non in fermentum. Euismod lectus tellus imperdiet amet condimentum semper nulla ipsum. Tortor ut vestibulum diam maecenas elementum viverra. Sed arcu integer sagittis feugiat diam egestas.</p>
+               <a href="#" class="btn gk-bg-primary700 w-100 text-white">Pilih Jalur Pendakian</a>
+            </div>
+         </div>
+   </div>
+   @endfor
+</div>
 </div>
 
-<div class="container">
-   badan
-</div>
-<div class="container">
-   gate
+
+<div class="gk-bg-neutrals200">
+   <div class="container">
+      <p>ejfbwjfbkjwbfwvbkn j njwsfbjwe</p>
+   </div>
 </div>
 
 @endsection
