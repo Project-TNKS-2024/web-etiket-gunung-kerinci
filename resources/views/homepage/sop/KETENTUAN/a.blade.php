b3ac29c35988fab546234b2e18286367829b0faa
yotadaa <mukhtadanasution@gmail.com>
2024-07-06 22:38:41 +0700

    changed sop to written, added some feature. changed panduan controller.

---
+++ resources/views/homepage/sop/KETENTUAN/a.blade.php
@@ -0,0 +1,49 @@
+<dt class="chapter-subheader" id="ketentuan-a">A. Ketentuan</dt>
+<dd class="chapter-content">
+    <ol class="d-flex flex-column gap-2">
+        <li>Pembelian karcis di Kantor Balai Besar TNKS Sungai Penuh, Pusat informasi R10 Kersik Tuo Kabupaten Kerinci
+            Provinsi Jambi untuk pendakian melalui pintu masuk Kersik Tuo sedangkan pendakian yang melalui pintu masuk
+            Camping Ground Bangun Rejo Kabupaten Solok Selatan Provinsi Sumatera Barat menggunakan Karcis dan SIMAKSI;
+        </li>
+        <li>Pengurusan SIMAKSI dilakukan secara online dan langsung di Kantor BBTNKS di Sungai Penuh atau Kantor Bidang
+            Pengelolaan TN Wilayah II Sumatera Barat di Padang atau Kantor Seksi Pengelolaan TN Wilayah IV Sangir dan
+            dapat dilakukan 2 bulan hingga 1 hari sebelum pendakian;</li>
+        <li>Batas waktu pendakian melalui Pos R10 Kersik Tuo Kabupaten Kerinci Provinsi Jambi adalah 2 (dua) hari dan
+            pendakian melalui Camping Ground Bangun Rejo Kabupaten Solok Selatan Provinsi Sumatera Barat adalah 4
+            (empat) hari, apabila merencanakan lebih dari itu <b>wajib</b> memberitahukan/ menginformasikan diawal
+            keberangkatan dan dikenai biaya kelipatannya;</li>
+        <li>Pendaki <b>wajib</b> memberitahukan rencana tanggal dan lokasi turun/kembali dan melaporkan kepada petugas
+            di pintu
+            masuk;</li>
+        <li>Tanggung jawab dan keselamatan pendaki menjadi tanggung jawab pribadi dan tidak akan menuntut pihak
+            pengelola (Balai Besar TNKS), pemandu, porter, pemerintah dan pemerintah daerah;</li>
+        <li>Bagi pendaki yang naik dari pintu masuk Kersik Tuo dan turun melalui pintu masuk Camping Ground Bangun Rejo
+            Kabupaten Solok Selatan <b>wajib</b> menggunakan SIMAKSI;</li>
+        <li>Apabila pendaki melanggar kelebihan hari dalam mendaki akan dikenakan SANKSI, kecuali dalam keadaan darurat
+            dan telah dilaporkan kepada petugas;</li>
+        <li>Jumlah pendaki yang diijinkan naik maksimum 46 orang/hari khusus jalur pendakian melalui Camping Ground
+            Bangun Rejo Kabupaten Solok Selatan Provinsi Sumatera Barat;</li>
+        <li>Setiap SIMAKSI pendakian dikeluarkan untuk grup pendaki dengan jumlah minimum 3 (tiga) orang dan maksimum 10
+            (sepuluh) orang pendaki, serta untuk mengurangi resiko kecelakaan diwajibkan menggunakan jasa pemanduan dari
+            Sekretariat Bersama Pemandu Gunung Kerinci Kabupaten Solok Selatan yang telah mendapat rekomendasi dari
+            Balai Besar TNKS;</li>
+        <li>Proses pengambilan SIMAKSI bisa dilakukan pada hari jam/ kerja;</li>
+        <li>Semua calon pendaki <b>wajib</b> menyerahkan fotokopi Kartu Identitas yang masih berlaku seperti: SIM, KTA,
+            KTP,
+            Paspor, atau Kartu Pelajar serta Surat Keterangan Sehat dokter (asli);</li>
+        <li>Calon pendaki yang berumur kurang dari 17 tahun, <b>wajib</b> menyerahkan Surat Izin Orang Tua/ Wali dan
+            ditandatangani di atas materai Rp. 10.000 serta dilampirkan fotokopi KTP Orang Tua/ Wali yang masih berlaku;
+        </li>
+        <li>Bagi Warga Negara Asing (WNA) yang akan mendaki <b>diwajibkan</b> untuk menggunakan jasa pemanduan yang
+            telah
+            terdaftar di Balai Besar TNKS;</li>
+        <li>Untuk komposisi jumlah pengunjung, pemandu yaitu:
+            <ol type="a">
+                <li>Maksimal pemanduan: 1 orang pemandu untuk 5 orang pengunjung/pendaki</li>
+                <li>Maksimal beban porter: 1 orang porter seberat 25 Kg</li>
+            </ol>
+        </li>
+        <li>Membayar karcis masuk sesuai dengan peraturan yang berlaku dan asuransi keselamatan</li>
+    </ol>
+
+</dd>
