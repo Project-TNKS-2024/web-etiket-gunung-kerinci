c6f9a2cba1033baf6d07d870870b9b69faee1138
yotadaa <mukhtadanasution@gmail.com>
2024-10-24 23:09:10 +0700

    test

--- resources/views/etiket/admin/master-data/tiket/edit.blade.php
+++ resources/views/etiket/admin/master-data/tiket/edit.blade.php
@@ -25,11 +25,11 @@
             @if ($errors->any())
             <div class="row">
                 <div class="col btn btn-danger">
-                    @error('destinasi') Destinasi tidak valid  @enderror
-                    @error('tipe') tipe tidak valid  @enderror
-                    @error('jenis') jenis tidak valid  @enderror
-                    @error('gate') gate tidak valid  @enderror
-                    @error('hargaTiket') Harga Tiket tidak valid  @enderror
+                    @error('destinasi') Destinasi tidak valid @enderror
+                    @error('tipe') tipe tidak valid @enderror
+                    @error('jenis') jenis tidak valid @enderror
+                    @error('gate') gate tidak valid @enderror
+                    @error('hargaTiket') Harga Tiket tidak valid @enderror
                 </div>
             </div>
             @endif
@@ -38,42 +38,56 @@
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Paket Tiket</label>
                     <div class="dropdown w-100">
-                        <button class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between  align-items-center dropdown-toggle" type="button"  data-bs-toggle="dropdown" aria-expanded="false" style="border-color: var(--neutrals700);  overflow-x: hidden">
-                            <span id="paket_tiket" style=" overflow-x: hidden">{{$data->paket_tiket->nama}}</span>
+                        <button
+                            class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between  align-items-center dropdown-toggle"
+                            type="button" data-bs-toggle="dropdown" aria-expanded="false"
+                            style="border-color: var(--neutrals700);  overflow-x: hidden">
+                            <span id="paket_tiket" style=" overflow-x: hidden">{{$data->paket_tiket->nama ?
+                                $data->paket_tiket->nama : "" }}</span>
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="destinasi">
                             @foreach ($paket_tiket as $item )
-                                <li><a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item" onclick="gantiPaket(event, 'paket_tiket','paket-value', '{{$item}}', '{{$item->destinasi}}')" href="#">{{$item->nama}}</a></li>
+                            <li><a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item"
+                                    onclick="gantiPaket(event, 'paket_tiket','paket-value', '{{$item}}', '{{$item->destinasi}}')"
+                                    href="#">{{$item->nama}}</a></li>
                             @endforeach
                         </ul>
                     </div>
                 </div>
-                <input type="hidden" id="paket-value" name="paket_tiket" value="{{$data->id}}"/>
+                <input type="hidden" id="paket-value" name="paket_tiket" value="{{$data->id}}" />
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Destinasi</label>
-                    <div class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between align-items-center" style="border-color: var(--neutrals700)" id="destinasi-label">
+                    <div class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between align-items-center"
+                        style="border-color: var(--neutrals700)" id="destinasi-label">
                         {{$data->paket_tiket->destinasi->nama}}
                     </div>
                 </div>
-                <input name="destinasi" id="destinasi-value" value="{{$data->paket_tiket->destinasi->id}}" type="hidden"/>
+                <input name="destinasi" id="destinasi-value" value="{{$data->paket_tiket->destinasi->id}}"
+                    type="hidden" />
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Nama Tiket</label>
-                    <input disabled id="nama-tiket" name="nama" value="{{$data->paket_tiket->nama}}" class="form-control " style="border: 1px solid var(--neutrals500)"/>
+                    <input disabled id="nama-tiket" name="nama" value="{{$data->paket_tiket->nama}}"
+                        class="form-control " style="border: 1px solid var(--neutrals500)" />
                 </div>
             </div>
             <div class="row gap-0">
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Keterangan</label>
-                    <textarea disabled id="keterangan-tiket" name="keterangan" placeholder="Keterangan" class="form-control" style="border: 1px solid var(--neutrals500);" rows="1">{{$data->paket_tiket->keterangan}}</textarea>
+                    <textarea disabled id="keterangan-tiket" name="keterangan" placeholder="Keterangan"
+                        class="form-control" style="border: 1px solid var(--neutrals500);"
+                        rows="1">{{$data->paket_tiket->keterangan}}</textarea>
                 </div>
 
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Penugasan</label>
-                    <input disabled id="penugasan-tiket" name="penugasan" value="{{$data->paket_tiket->penugasan}}" placeholder="Penugasan" class="form-control" style="border: 1px solid var(--neutrals500)"/>
+                    <input disabled id="penugasan-tiket" name="penugasan" value="{{$data->paket_tiket->penugasan}}"
+                        placeholder="Penugasan" class="form-control" style="border: 1px solid var(--neutrals500)" />
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Minimal Pendaki</label>
-                    <input disabled id="minimal-tiket" min="1" type="number" name="penugasan" value="{{$data->paket_tiket->min_pendaki}}" placeholder="Penugasan" class="form-control " style="border: 1px solid var(--neutrals500)"/>
+                    <input disabled id="minimal-tiket" min="1" type="number" name="penugasan"
+                        value="{{$data->paket_tiket->min_pendaki}}" placeholder="Penugasan" class="form-control "
+                        style="border: 1px solid var(--neutrals500)" />
 
                 </div>
             </div>
@@ -82,72 +96,97 @@
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Kategori Pendaki</label>
                     <div class="dropdown w-100">
-                        <button class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between  align-items-center dropdown-toggle" type="button" id="kategori_pendaki" data-bs-toggle="dropdown" aria-expanded="false" style="border-color: var(--neutrals700)">
+                        <button
+                            class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between  align-items-center dropdown-toggle"
+                            type="button" id="kategori_pendaki" data-bs-toggle="dropdown" aria-expanded="false"
+                            style="border-color: var(--neutrals700)">
                             {{$data->kategori_pendaki == "wna" ? "Mancanegara" : "Nusantara"}}
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="destinasi">
                             <li>
-                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item" onclick="select(event, 'kategori_pendaki','kategori-pendaki-value', 'wna')" href="#">Mancanegara</a>
+                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item"
+                                    onclick="select(event, 'kategori_pendaki','kategori-pendaki-value', 'wna')"
+                                    href="#">Mancanegara</a>
                             </li>
                             <li>
-                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item" onclick="select(event, 'kategori_pendaki','kategori-pendaki-value', 'wni')" href="#">Nusantara</a>
+                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item"
+                                    onclick="select(event, 'kategori_pendaki','kategori-pendaki-value', 'wni')"
+                                    href="#">Nusantara</a>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <input type="hidden" id="kategori-pendaki-value" name="kategori_pendaki" value="{{$data->kategori_pendaki}}"/>
+                <input type="hidden" id="kategori-pendaki-value" name="kategori_pendaki"
+                    value="{{$data->kategori_pendaki}}" />
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Kategori Hari</label>
                     <div class="dropdown w-100">
-                        <button class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between  align-items-center dropdown-toggle" type="button" id="kategori_hari" data-bs-toggle="dropdown" aria-expanded="false" style="border-color: var(--neutrals700)">
+                        <button
+                            class="btn btn-outline gk-text-neutrals700 w-100 text-start d-flex justify-content-between  align-items-center dropdown-toggle"
+                            type="button" id="kategori_hari" data-bs-toggle="dropdown" aria-expanded="false"
+                            style="border-color: var(--neutrals700)">
                             {{$data->kategori_hari == "wd" ? "Weekday" : "Weekend"}}
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="destinasi">
                             <li>
-                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item" onclick="select(event, 'kategori_hari','kategori-hari-value', 'wd')" href="#">Weekday</a>
+                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item"
+                                    onclick="select(event, 'kategori_hari','kategori-hari-value', 'wd')"
+                                    href="#">Weekday</a>
                             </li>
                             <li>
-                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item" onclick="select(event, 'kategori_hari','kategori-hari-value', 'wk')" href="#">Weekend</a>
+                                <a class="{{$data->id == $item->id ? 'bg-primary text-white' : ''}} dropdown-item"
+                                    onclick="select(event, 'kategori_hari','kategori-hari-value', 'wk')"
+                                    href="#">Weekend</a>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <input type="hidden" id="kategori-hari-value" name="kategori_hari" value="{{$data->kategori_hari}}"/>
-                <input name="destinasi" id="destinasi-value" value="{{$data->paket_tiket->destinasi->id}}" type="hidden"/>
+                <input type="hidden" id="kategori-hari-value" name="kategori_hari" value="{{$data->kategori_hari}}" />
+                <input name="destinasi" id="destinasi-value" value="{{$data->paket_tiket->destinasi->id}}"
+                    type="hidden" />
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Harga Masuk</label>
-                    <div style="border: 1px solid var(--neutrals500);" class="rounded px-2 d-flex align-items-center gap-3">
+                    <div style="border: 1px solid var(--neutrals500);"
+                        class="rounded px-2 d-flex align-items-center gap-3">
                         <div>Rp</div>
-                        <input id="harga-tiket" name="nama" value="{{$data->harga_masuk}}" class="form-control text-end" min="0" style="background-color: transparent;border: none"/>
+                        <input id="harga-tiket" name="nama" value="{{$data->harga_masuk}}" class="form-control text-end"
+                            min="0" style="background-color: transparent;border: none" />
                     </div>
                 </div>
             </div>
             <div class="row gap-0">
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Harga Kemah</label>
-                    <div style="border: 1px solid var(--neutrals500);" class="rounded px-2 d-flex align-items-center gap-3">
+                    <div style="border: 1px solid var(--neutrals500);"
+                        class="rounded px-2 d-flex align-items-center gap-3">
                         <div>Rp</div>
-                        <input id="harga-kemah" name="harga_kemah" value="{{$data->harga_kemah}}" class="form-control text-end" min="0" style="background-color: transparent;border: none"/>
+                        <input id="harga-kemah" name="harga_kemah" value="{{$data->harga_kemah}}"
+                            class="form-control text-end" min="0" style="background-color: transparent;border: none" />
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Harga Traking</label>
-                    <div style="border: 1px solid var(--neutrals500);" class="rounded px-2 d-flex align-items-center gap-3">
+                    <div style="border: 1px solid var(--neutrals500);"
+                        class="rounded px-2 d-flex align-items-center gap-3">
                         <div>Rp</div>
-                        <input id="harga-traking" name="harga_traking" value="{{$data->harga_traking}}" class="form-control text-end" min="0" style="background-color: transparent;border: none"/>
+                        <input id="harga-traking" name="harga_traking" value="{{$data->harga_traking}}"
+                            class="form-control text-end" min="0" style="background-color: transparent;border: none" />
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <label class="form-label p-0 m-0">Harga Asuransi</label>
-                    <div style="border: 1px solid var(--neutrals500);" class="rounded px-2 d-flex align-items-center gap-3">
+                    <div style="border: 1px solid var(--neutrals500);"
+                        class="rounded px-2 d-flex align-items-center gap-3">
                         <div>Rp</div>
-                        <input id="harga-asuransi" name="harga_asuransi" value="{{$data->harga_ansuransi}}" class="form-control text-end" min="0" style="background-color: transparent;border: none"/>
+                        <input id="harga-asuransi" name="harga_asuransi" value="{{$data->harga_ansuransi}}"
+                            class="form-control text-end" min="0" style="background-color: transparent;border: none" />
                     </div>
                 </div>
             </div>
             <div class="row gap-2 mt-3">
                 <div class="col-md-3 col-sm-6">
-                    <button type="submit" class="btn font-bold text-black btn-outline w-fit text-start shadow" style="border-color: var(--neutrals700)">
+                    <button type="submit" class="btn font-bold text-black btn-outline w-fit text-start shadow"
+                        style="border-color: var(--neutrals700)">
                         <i class="ti ti-device-floppy gk-text-primary600"></i>
                         Simpan
                     </button>
@@ -177,4 +216,3 @@
     </script>
 </div>
 @endsection
-
--- resources/views/etiket/user/sections/dashboard.blade.php
+++ resources/views/etiket/user/sections/dashboard.blade.php
@@ -10,22 +10,25 @@
 <div class="col py-5 px-4 my-5 my-md-0" style="min-height: 700px; overflow-y: auto;">
     <h3 class="font-semibold">Selamat datang di website E-Tiket TNKS, {{auth()->user()->fullname}}</h3>
     <div class="row gap-3 px-2">
-        <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 col-12 text-sm p-2 rounded py-3 d-flex gap-3" style="border: 1px solid var(--neutrals300)">
-            <div><img src="{{asset('assets/icon/tnks/compass-dark.svg')}}" width="40"/></div>
+        <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 col-12 text-sm p-2 rounded py-3 d-flex gap-3"
+            style="border: 1px solid var(--neutrals300)">
+            <div><img src="{{asset('assets/icon/tnks/compass-dark.svg')}}" width="40" /></div>
             <div>
                 <div>Explorasi Selesasi</div>
                 <div>1 Explorasi</div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 col-12 text-sm p-2 rounded py-3 d-flex gap-3" style="border: 1px solid var(--neutrals300)">
-            <div><img src="{{asset('assets/icon/tnks/compass-dark.svg')}}" width="40"/></div>
+        <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 col-12 text-sm p-2 rounded py-3 d-flex gap-3"
+            style="border: 1px solid var(--neutrals300)">
+            <div><img src="{{asset('assets/icon/tnks/compass-dark.svg')}}" width="40" /></div>
             <div>
                 <div>Status Pendaftaran</div>
                 <div>1 Explorasi</div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 col-12 text-sm p-2 rounded py-3 d-flex gap-3" style="border: 1px solid var(--neutrals300)">
-            <div><img src="{{asset('assets/icon/tnks/compass-dark.svg')}}" width="40"/></div>
+        <div class="col-sm-12 col-md-12 col-lg-5 col-xl-3 col-12 text-sm p-2 rounded py-3 d-flex gap-3"
+            style="border: 1px solid var(--neutrals300)">
+            <div><img src="{{asset('assets/icon/tnks/compass-dark.svg')}}" width="40" /></div>
             <div>
                 <div>Hitung Mundur Check-In</div>
                 <div>1 Explorasi</div>
@@ -33,72 +36,76 @@
         </div>
     </div>
     <h4 class="font-semibold mt-4 px-3">Riwayat Booking</h4>
-    <div class="d-flex gap-5 px-3" style="flex-wrap: wrap;">
+    <div class="d-flex gap-5 px-3" style="width:120vh; overflow-x: auto;">
         @foreach ($booking as $book)
-            <div class="row col-5 col-sm-12 col-md-12 col-lg-12 col-xl-6 rounded-2xl card ticket px-4 p-sm-4 p-md-5 pt-md-3 pt-3" style="padding-bottom: 30px; padding-right: 30px;">
-                <div class="row align-items-center gap-2 font-semibold">Status <div class="{{$book->status_pembayaran ? "gk-bg-success200" : "gk-bg-warning200"}} rounded-md px-4 py-2 font-bold" style="width: fit-content">{{$book->status_pembayaran ? "Aktif" : "Belum Bayar"}}</div></div>
-                <div class="row ticket-content  mx-0">
-                    <div class="col-12">
-                        <div class="row my-2 ">
-                            <div class="col-6 ">
-                                <div class="font-semibold">Nama Ketua</div>
-                                <div class="">{{auth()->user()->fullname}}</div>
-                            </div>
-                            <div class="col-6 ">
-                                <div class="font-semibold">SIMAKSI</div>
-                                <div class="" style="color: red;">Tidak</div>
-                            </div>
+        <div class="row col-5 col-sm-12 col-md-12 col-lg-12 col-xl-6 rounded-2xl card ticket px-4 p-sm-4 p-md-5 pt-md-3 pt-3"
+            style="padding-bottom: 30px; padding-right: 30px;">
+            <div class="row align-items-center gap-2 font-semibold">Status <div class="{{$book->status_pembayaran ? "
+                    gk-bg-success200" : "gk-bg-warning200" }} rounded-md px-4 py-2 font-bold"
+                    style="width: fit-content">{{$book->status_pembayaran ? "Aktif" : "Belum Bayar"}}</div>
+            </div>
+            <div class="row ticket-content mx-0">
+                <div class="col-12">
+                    <div class="row my-2 ">
+                        <div class="col-6 ">
+                            <div class="font-semibold">Nama Ketua</div>
+                            <div class="">{{auth()->user()->fullname}}</div>
                         </div>
-                        <div class="row my-2">
-                            <div class="col-6">
-                                <div class="font-semibold">Gerbang Masuk</div>
-                                <div class="">{{$book->gateMasuk->nama}}</div>
-                            </div>
-                            <div class="col-6">
-                                <div class="font-semibold">Gerbang Keluar</div>
-                                <div class="">{{$book->gateKeluar->nama}}</div>
-                            </div>
-                        </div>
-
-                        <div class="row my-2">
-                            <div class="col-6">
-                                <div class="font-semibold">Check In</div>
-                                <div class="">{{$book->tanggal_masuk}}</div>
-                            </div>
-                            <div class="col-6">
-                                <div class="font-semibold">Check Out</div>
-                                <div class="">{{$book->tanggal_keluar}}</div>
-                            </div>
-                        </div>
-
-                        <div class="row my-2 ">
-                            <div class="col-6">
-                                <div class="font-semibold text-nowrap">Jumlah Anggota</div>
-                                <div class="">{{$book->total_pendaki_wni+$book->total_pendaki_wna}} Orang</div>
-                            </div>
-                            <div class="col-6" style="width: fit-content">
-                                <div class="font-semibold">Kewarganegaraan</div>
-                                <div class="row">
-                                    <div class="col">{{$book->total_pendaki_wni}} WNI</div>
-                                    <div class="col">{{$book->total_pendaki_wna}} WNA</div>
-                                </div>
-                            </div>
+                        <div class="col-6 ">
+                            <div class="font-semibold">SIMAKSI</div>
+                            <div class="" style="color: red;">Tidak</div>
                         </div>
                     </div>
-                    <div class="col-12 text-center font-semibold ">
-                        <img src="{{ asset('assets/img/sampel/QR.png') }}" style="max-width: 150px;" width="100%" />
-                        <div class="text-center w-100 text-sm mt-2">Kode Booking</div>
-                        <div class="text-center w-100 text-lg">{{$book->unique_code}}</div>
+                    <div class="row my-2">
+                        <div class="col-6">
+                            <div class="font-semibold">Gerbang Masuk</div>
+                            <div class="">{{$book->gateMasuk->nama}}</div>
+                        </div>
+                        <div class="col-6">
+                            <div class="font-semibold">Gerbang Keluar</div>
+                            <div class="">{{$book->gateKeluar->nama}}</div>
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="col-6">
+                            <div class="font-semibold">Check In</div>
+                            <div class="">{{$book->tanggal_masuk}}</div>
+                        </div>
+                        <div class="col-6">
+                            <div class="font-semibold">Check Out</div>
+                            <div class="">{{$book->tanggal_keluar}}</div>
+                        </div>
+                    </div>
+
+                    <div class="row my-2 ">
+                        <div class="col-6">
+                            <div class="font-semibold text-nowrap">Jumlah Anggota</div>
+                            <div class="">{{$book->total_pendaki_wni+$book->total_pendaki_wna}} Orang</div>
+                        </div>
+                        <div class="col-6" style="width: fit-content">
+                            <div class="font-semibold">Kewarganegaraan</div>
+                            <div class="row">
+                                <div class="col">{{$book->total_pendaki_wni}} WNI</div>
+                                <div class="col">{{$book->total_pendaki_wna}} WNA</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div class="col-12 text-center font-semibold ">
+                    <img src="{{ asset('assets/img/sampel/QR.png') }}" style="max-width: 150px;" width="100%" />
+                    <div class="text-center w-100 text-sm mt-2">Kode Booking</div>
+                    <div class="text-center w-100 text-lg">{{$book->unique_code}}</div>
+                </div>
             </div>
+        </div>
         @endforeach
     </div>
 
 </div>
 
-    <script>
-        console.log('users: ', @json($user));
+<script>
+    console.log('users: ', @json($user));
         let countriesData = [];
 
         const sidebarMenu = document.querySelectorAll(".dashboard-sidebar-btn");
@@ -107,15 +114,17 @@
         });
         const profile = document.querySelector("#dashboard");
         profile.classList.add("active");
-    </script>
+</script>
 @endsection
 
 @push('scripts')
 <!-- Popper.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.7/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybA6zjzLIpKflFsFVxYycpV5x9W4FUIsxAB7BJ4Vf8pZFOFpg" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.7/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz4fnFO9gybA6zjzLIpKflFsFVxYycpV5x9W4FUIsxAB7BJ4Vf8pZFOFpg" crossorigin="anonymous">
 </script>
 <!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9b0Lr1Bt6f3B6md62z0vT4Xdb1BzVg20r13a6c4a21fF3+k9J7l6+dB" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9b0Lr1Bt6f3B6md62z0vT4Xdb1BzVg20r13a6c4a21fF3+k9J7l6+dB" crossorigin="anonymous">
 </script>
 @endpush
 
