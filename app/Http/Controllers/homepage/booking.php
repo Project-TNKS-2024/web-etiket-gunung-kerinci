87dc8994ccc72d92fdaeb11b6f68cf32ed8d819d
Muktashim Billah <muktasim.elfatih@gmail.com>
2025-03-11 14:21:59 +0700

    comet

--- app/Http/Controllers/homepage/booking.php
+++ app/Http/Controllers/homepage/booking.php
@@ -223,7 +223,7 @@ class booking extends Controller
             return back()->withErrors(['code' => 'Anda sudah melakukan booking di tanggal tersebut']);
         }
 
-        return $pendakiHaveBooking;
+        // return $pendakiHaveBooking;
 
         // cari booking terakhir yang blm di verifikasi
         $booking = gk_booking::where('id_user', Auth::user()->id)
