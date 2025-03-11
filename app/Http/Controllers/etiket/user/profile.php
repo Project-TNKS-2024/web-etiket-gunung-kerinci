40c91488507c9adce2dd4fe7cb194fc34baae217
Muktashim Billah <muktasim.elfatih@gmail.com>
2025-03-11 12:54:20 +0700

    comet

--- app/Http/Controllers/etiket/user/profile.php
+++ app/Http/Controllers/etiket/user/profile.php
@@ -150,7 +150,7 @@ class profile extends Controller
             'lastName' => 'string|max:255|nullable',
             'lampiran_identitas' => 'required|file|mimes:jpg,jpeg,png,pdf|max:548',
             'kewarganegaraan' => 'required|string',
-            'nik' => 'required|numeric|min:8',
+            'nik' => 'required|string|min:8|max:16|alpha_num',
             'nomor_telepon' => 'required|numeric',
             'telp_country' => 'required|string|max:5',
             'jenis_kelamin' => 'required|in:l,p',
