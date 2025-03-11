ac9bae036c7f3acbd72b033612b4bc51562e6dd8
Muktashim Billah <muktasim.elfatih@gmail.com>
2025-03-11 15:18:38 +0700

    comet

--- resources/views/email/BioValidasi.blade.php
+++ resources/views/email/BioValidasi.blade.php
@@ -2,17 +2,19 @@
    # Halo, {{ $nama }}
 
    @if ($status == 'verified')
-   {!! '<b>Selamat!</b>' !!} Biodata Anda telah berhasil diverifikasi pada {!! '<b>' . $tanggalVerifikasi . '</b>' !!}.
+   ## Selamat!
+   Biodata Anda telah berhasil diverifikasi pada ** {{ $tanggalVerifikasi }} **.
    Anda sekarang dapat mengakses layanan kami sepenuhnya.
    @else
-   {!! '<b>Maaf,</b>' !!} biodata Anda tidak dapat diverifikasi pada {!! '<b>' . $tanggalVerifikasi . '</b>' !!}.
-   {!! '<b>Alasan:</b>' !!} {{ $keterangan }}
+   ## Maaf,
+   biodata Anda tidak dapat diverifikasi pada ** {{ $tanggalVerifikasi }} **.
+   ** Alasan: ** {{ $keterangan }}
    @endif
 
    <x-mail::button :url="$url">
       Lihat Detail
    </x-mail::button>
 
-   Terima kasih,
-   {!! '<b>' . config('app.name') . '</b>' !!}
+   Terima kasih,<br>
+   **{{ config('app.name') }}**
 </x-mail::message>
