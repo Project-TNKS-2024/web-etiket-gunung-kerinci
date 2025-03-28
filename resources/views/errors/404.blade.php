7f6d8d6762253fae33736a51d9d4eb348d7c4b6b
muktashimbillah <muktasim.elfatih@gmail.com>
2025-03-28 20:50:23 +0700

    laman 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,9 +7,8 @@
     <h5 class="mt-2 text-center fw-bold">HALAMAN TIDAK TERSEDIA</h5>
     <div>
         <p class="fw-bold text-center my-5">Periksa kembali alamat URL</p>
-        <a class="btn btn-primary gk-bg-primary700 py-2 w-100" href='#' onclick="history.back()">Kembali ke Beranda</a>
-    </div>
-</div>
+        <a class="btn btn-primary gk-bg-primary700 py-2 w-100" href="{{ route('homepage') }}" onclick="event.preventDefault(); if(window.history.length > 1) { history.back(); } else { window.location.href='{{ route('homepage') }}'; }">Kembali ke Beranda</a>
+    </div></div>
 @endsection
 
 @section('js')
