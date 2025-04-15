d67ce1d75c8dd525a1e12fcfcae10141ec24e9d5
muktashimbillah <muktasim.elfatih@gmail.com>
2025-04-15 22:52:13 +0700

    err

--- resources/views/email/BioValidasi.blade.php
+++ resources/views/email/BioValidasi.blade.php
@@ -2,27 +2,8 @@
    # Halo, {{ $nama }}
 
    @if ($status == 'verified')
-   **Selamat!** Biodata Anda telah berhasil diverifikasi pada **{{ $tanggalVerifikasi }}**.
-   Anda sekarang dapat mengakses layanan kami sepenuhnya.
-   @else
-   **Maaf,** biodata Anda tidak dapat diverifikasi pada **{{ $tanggalVerifikasi }}**.
-   **Alasan:** {{ $keterangan }}
-   @endif
-
-   <x-mail::button :url="$url">
-      Lihat Detail
-   </x-mail::button>
-
-   Terima kasih,<br>
-   **{{ config('app.name') }}**
-</x-mail::message>
-
-
-<x-mail::message>
-   # Halo, {{ $nama }}
-
-   @if ($status == 'verified')
    Selamat!
+
    Akun Anda telah berhasil diverifikasi pada {{ $tanggalVerifikasi }}.
    Anda kini mendapatkan ID Pendaki, yang dapat dilihat di halaman Profil dan digunakan untuk pemesanan tiket.
    @else
