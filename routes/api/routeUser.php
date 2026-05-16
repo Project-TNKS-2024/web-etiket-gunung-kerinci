ab6ce67cc07cc4aef64b1de249e06d2980d62015
yotadaa <mukhtadanasution@gmail.com>
2026-05-16 14:50:24 +0700

    add id pendaki and status verifikasi endpoint

--- routes/api/routeUser.php
+++ routes/api/routeUser.php
@@ -8,6 +8,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // beranda
     Route::get('/beranda', [HomeController::class, 'beranda'])->name('api.beranda');
     // api profile
+    Route::get('/profile/pendaki-identity', [ProfileController::class, 'getPendakiIdentity'])->name('api.profile.pendaki-identity');
     Route::get('/profile/getbiodata', [ProfileController::class, 'getBiodata'])->name('api.profile.getbiodata');
     Route::post('/profile/updatebiodata', [ProfileController::class, 'updateBiodata'])->name('api.profile.updatebiodata');
     Route::post('/profile/gantipassword', [ProfileController::class, 'gantiPassword'])->name('api.profile.gantipassword');
